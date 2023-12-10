ac3ce1a9d624cdd5711c2e07ec62897e6799c513
Igor <drugakovigor751@gmail.com>
2023-12-10 12:38:13 +0300

    Create CoinsController.cs

---
+++ CoinsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsController : MonoBehaviour
+{
+    [SerializeField] private AudioClip coinClip;
+    [SerializeField] private AudioClip crystalClip;
+    private AudioSource audioSource;
+    [SerializeField] private Text coinText;
+    [SerializeField] private Text crystalText;
+    private int coinQuantity;
+    private int crystalQuantity;
+
+    private void Awake()
+    {
+        coinQuantity = int.Parse(coinText.text);
+        crystalQuantity = int.Parse(crystalText.text);
+        audioSource = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Coin"))
+        {
+            audioSource.clip = coinClip;
+            audioSource.Play();
+            Destroy(collision.gameObject);
+            coinQuantity++;
+            coinText.text = coinQuantity.ToString();
+        }
+        else if (collision.CompareTag("Crystal"))
+        {
+            audioSource.clip = crystalClip;
+            audioSource.Play();
+            Destroy(collision.gameObject);
+            crystalQuantity++;
+            crystalText.text = crystalQuantity.ToString();
+        }
+    }
+}
