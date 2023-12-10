d81c63870b97fae4366249d9ca38786dc3319301
Igor <drugakovigor751@gmail.com>
2023-12-10 12:37:13 +0300

    Create BackpackHelp.cs

---
+++ BackpackHelp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackpackHelp : MonoBehaviour
+{
+    [SerializeField] private Health health;
+    [SerializeField] private Shooter shooter;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hero"))
+        {
+            health.HPHelp();
+            shooter.BulletHelp();
+            Destroy(gameObject);
+        }
+    }
+}
