7c2f3353ce9b277c8af083410e25bfdb190d18f5
Igor <drugakovigor751@gmail.com>
2023-12-10 12:40:43 +0300

    Create FireballDamageDiller.cs

---
+++ FireballDamageDiller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballDamageDiller : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Health health = collision.gameObject.GetComponent<Health>();
+        if (health != null && collision.CompareTag("Hero"))
+        {
+            health.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
