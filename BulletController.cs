12f5b4ef5d69bfae7315c946192c6d7f28971c60
Igor <drugakovigor751@gmail.com>
2023-12-10 12:37:35 +0300

    Create BulletController.cs

---
+++ BulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField] private Text bulletText;
+    private Shooter shooter;
+
+    private void Awake()
+    {
+        shooter = GetComponent<Shooter>();
+        bulletText.text = shooter.CurrentBulletValue.ToString();
+    }
+
+    private void OnEnable()
+    {
+        shooter.BulletValueChanged += HandleBulletValueChanged;
+    }
+
+    private void OnDisable()
+    {
+        shooter.BulletValueChanged -= HandleBulletValueChanged;
+    }
+
+    private void HandleBulletValueChanged()
+    {
+        bulletText.text = shooter.CurrentBulletValue.ToString();
+    }
+}
