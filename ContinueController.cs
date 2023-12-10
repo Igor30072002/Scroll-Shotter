265a83923a0bdd9d1913ca3f205092231ed44637
Igor <drugakovigor751@gmail.com>
2023-12-10 12:38:24 +0300

    Create ContinueController.cs

---
+++ ContinueController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] characters;
+    [SerializeField] private GameObject hero;
+    [SerializeField] private GameObject gamePanel;
+    [SerializeField] private PauseController pauseController;
+    public void Continue()
+    {
+        foreach (GameObject character in characters)
+            character.SetActive(true);
+        pauseController.InPause = false;
+        hero.SetActive(true);
+        gamePanel.SetActive(true);
+    }
+}
