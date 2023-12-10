853e9b1f0b11b821767099fe52302f85214d5aaa
Igor <drugakovigor751@gmail.com>
2023-12-10 12:35:28 +0300

    Create BackgroundMusicScript.cs

---
+++ BackgroundMusicScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackgroundMusicScript : MonoBehaviour
+{
+    [SerializeField] private AudioClip MenuClip;
+    [SerializeField] private AudioClip LevelClip;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioSource>();
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            audioSource.clip = MenuClip;
+            audioSource.Play();
+        }
+        else if(SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            audioSource.clip = LevelClip;
+            audioSource.Play();
+        }
+    }
+}
