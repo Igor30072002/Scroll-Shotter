61529b90728320fa8aa7ed508a25e06669793ad9
Igor <drugakovigor751@gmail.com>
2023-12-10 12:41:53 +0300

    Create PillController.cs

---
+++ PillController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillController : MonoBehaviour
+{
+    [SerializeField] private AudioClip pillClip;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hero"))
+        {
+            audioSource.clip = pillClip;
+            audioSource.Play();
+            collision.gameObject.GetComponent<Health>().HPHelp();
+            Destroy(gameObject);
+        }
+    }
+}
