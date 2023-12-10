460874a61db6d0d5b3b67a45026fff47d3a55873
Igor <drugakovigor751@gmail.com>
2023-12-10 12:41:47 +0300

    Create PauseController.cs

---
+++ PauseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    private bool inPause;
+    public bool InPause
+    {
+        get { return inPause; }
+        set { inPause = value; }
+    }
+    [SerializeField] private GameObject[] characters;
+    [SerializeField] private GameObject hero;
+    [SerializeField] private GameObject gamePanel;
+    [SerializeField] private Shooter heroShooter;
+
+    private void Awake()
+    {
+        inPause = false;
+    }
+
+    public void Pause()
+    {
+        foreach(GameObject character in characters)
+            character.SetActive(false);
+        inPause = true;
+        hero.SetActive(false);
+        gamePanel.SetActive(false);
+        heroShooter.CurrentBulletValue++;
+    }
+}
