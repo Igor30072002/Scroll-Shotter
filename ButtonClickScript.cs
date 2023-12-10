5d733a868daf8e33ad4d95a6483cd58553146915
Igor <drugakovigor751@gmail.com>
2023-12-10 12:37:51 +0300

    Create ButtonClickScript.cs

---
+++ ButtonClickScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClickScript : MonoBehaviour
+{
+    [SerializeField] private AudioClip buttonClip;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<AudioSource>();
+    }
+
+    public void Click()
+    {
+        audioSource.clip = buttonClip;
+        audioSource.Play();
+    }
+}
