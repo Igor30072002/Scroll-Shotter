d0f1c037e45717822e785a822dcbb9dd32af0733
Igor <drugakovigor751@gmail.com>
2023-12-10 12:38:46 +0300

    Create CurrentHeroHP.cs

---
+++ CurrentHeroHP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentHeroHP : MonoBehaviour
+{
+    [SerializeField] private Text HPText;
+    private Health health;
+
+    private void Awake()
+    {
+        health = GetComponent<Health>();
+        HPText.text = health.CurrentHealth.ToString();
+    }
+    private void OnEnable()
+    {
+        health.HealthValueChanged += HandleHealthValueChanged;
+    }
+
+    private void OnDisable()
+    {
+        health.HealthValueChanged -= HandleHealthValueChanged;
+    }
+
+    private void HandleHealthValueChanged()
+    {
+        HPText.text = health.CurrentHealth.ToString();
+    }
+}
