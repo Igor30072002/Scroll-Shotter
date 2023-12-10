ca8171e70f35392d992770e25395f0351c0d62be
Igor <drugakovigor751@gmail.com>
2023-12-10 12:41:12 +0300

    Create LavaDamage.cs

---
+++ LavaDamage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaDamage : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    private Health health;
+    private bool inLava;
+    private float timer;
+
+    private void Awake()
+    {
+        health = GetComponent<Health>();
+        inLava = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Lava"))
+        {
+            inLava = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Lava"))
+        {
+            inLava = false;
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (inLava && timer > 1)
+        {
+            health.TakeDamage(damage);
+            timer = 0;
+        }
+    }
+}
