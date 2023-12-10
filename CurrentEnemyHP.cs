0375cd3228cfd2a46f4f5af29e1166c3056181d8
Igor <drugakovigor751@gmail.com>
2023-12-10 12:38:34 +0300

    Create CurrentEnemyHP.cs

---
+++ CurrentEnemyHP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentEnemyHP : MonoBehaviour
+{
+    [SerializeField] private Text HPText;
+    private Health health;
+
+    private void Start()
+    {
+        health = GetComponent<Health>();
+    }
+
+    private void OnEnable()
+    {
+        health.HealthValueChanged += HandleHealthValueChanged;
+    }
+
+    private void OnDisable()
+    {
+        health.HealthReachedZero -= HandleHealthValueChanged;
+    }
+
+    private void HandleHealthValueChanged()
+    {
+        HPText.text = health.CurrentHealth.ToString();
+    }
+}
