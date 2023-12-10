c336526e92a1c1d89046a7e03f8574dac5e59ded
Igor <drugakovigor751@gmail.com>
2023-12-10 12:41:33 +0300

    Create OpenSettingsController.cs

---
+++ OpenSettingsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSettingsController : MonoBehaviour
+{
+    [SerializeField] private RectTransform settingsPanel;
+
+    public void Click()
+    {
+        settingsPanel.localScale = Vector3.one;
+    }
+}
