773eeb284847dda7bba3cf8f88859b28f119eaf0
Igor <drugakovigor751@gmail.com>
2023-12-10 12:41:40 +0300

    Create ParallaxController.cs

---
+++ ParallaxController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    [SerializeField] private Transform background;
+    [SerializeField] private float coeff;
+    private void Update()
+    {
+        background.position = transform.position * coeff;
+    }
+}
