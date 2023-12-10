a46c0fe14f7363119d618157307e7cbff0576b02
Igor <drugakovigor751@gmail.com>
2023-12-10 12:41:27 +0300

    Create LossScript.cs

---
+++ LossScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LossScript : MonoBehaviour
+{
+    [SerializeField] private GameObject LosePanel;
+    [SerializeField] private GameObject GamePanel;
+    [SerializeField] private Health health;
+
+    private void OnEnable()
+    {
+        health.HealthReachedZero += HandleHealthReachedZero;
+    }
+
+    private void OnDisable()
+    {
+        health.HealthReachedZero -= HandleHealthReachedZero;
+    }
+
+    private void HandleHealthReachedZero()
+    {
+        LosePanel.SetActive(true);
+        GamePanel.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
